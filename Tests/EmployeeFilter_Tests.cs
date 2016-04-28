7f74697469cdf9b51588477182b7a496a00cd3d5
Joshua Rakita <rakitaj@users.noreply.github.com>
2016-04-28 11:13:45 -0700

    Created some employee name filter tests.

---
+++ Tests/EmployeeFilter_Tests.cs
@@ -0,0 +1,30 @@
+﻿using designIssueExample.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designIssueExample.Tests
+{
+    [TestClass()]
+    public class EmployeeFilter_Tests
+    {
+        [TestMethod()]
+        public void TommyBoy__Should_Match_Starts_With_Tom()
+        {
+            Employee employee1 = new Employee(15, "Tommy Boy", 26, true);
+            EmployeeNameFilter filter = new EmployeeNameFilter("Tom");
+            Assert.IsTrue(filter.MatchesFilter(employee1));
+        }
+
+        [TestMethod()]
+        public void Tommy_Boy__Should_Not_Match_Starts_With_A()
+        {
+            Employee employee1 = new Employee(15, "Tommy Boy", 26, true);
+            EmployeeNameFilter filter = new EmployeeNameFilter("A");
+            Assert.IsFalse(filter.MatchesFilter(employee1));
+        }
+    }
+}
