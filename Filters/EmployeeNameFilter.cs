aca6b56b12a5bb6b8071cf00c4923437b0eff72d
Joshua Rakita <rakitaj@users.noreply.github.com>
2016-04-28 11:08:37 -0700

    EmployeeNameFilter class created. Using constructor to hold value.

--- Filters/EmployeeNameFilter.cs
+++ Filters/EmployeeNameFilter.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace designIssueExample.Filters
 {
-    class EmployeeNameFilter
+    public class EmployeeNameFilter : EmployeeFilter
     {
+        public String NameStartsWith { get; private set; }
+
+        public EmployeeNameFilter(string nameStartsWith)
+        {
+            this.NameStartsWith = nameStartsWith;
+        }
+
+        public override bool MatchesFilter(Employee employee)
+        {
+            bool result = false;
+            if(employee.Name.StartsWith(this.NameStartsWith))
+            {
+                result = true;
+            }
+            return result;
+        }
     }
 }
