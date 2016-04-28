be700108ff186c1509d6c656cb5592787d1b8f99
Joshua Rakita <rakitaj@users.noreply.github.com>
2016-04-28 10:54:17 -0700

    Made constructor for Employees and properties private set.

--- Employee.cs
+++ Employee.cs
@@ -3,10 +3,18 @@ namespace designIssueExample
 {
     class Employee
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public bool IsSalaried { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public bool IsSalaried { get; private set; }
+
+        public Employee(int id, string name, int age, bool isSalaried)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.IsSalaried = isSalaried;
+        }
 
         public override string ToString()
         {
