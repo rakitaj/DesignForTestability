f655b37a6e2d5c29cd17326c2543cd78bab4ad14
Joshua Rakita <rakitaj@users.noreply.github.com>
2016-04-28 10:43:45 -0700

    Created pinning tests for both types of filters.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace designIssueExample
         static void Main(string[] args)
         {
             Yucky yucky = new Yucky();
-            var employees = yucky.GetEmployees(EmployeeFilterType.ByName, "T", new FakeSqlConnection());
+            //var employees = yucky.GetEmployees(EmployeeFilterType.ByName, "T", new FakeSqlConnection());
+            var employees = yucky.GetEmployees(EmployeeFilterType.ExemptOnly, null, new FakeSqlConnection());
 
             foreach (Employee employee in employees)
             {
---
+++ Tests/Yucky_Pinning_Tests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designIssueExample.Tests
+{
+    [TestClass()]
+    public class Yucky_Pinning_Tests
+    {
+        [TestMethod()]
+        public void Program_Run_With_FilterType_Name_And_Filter_T()
+        {
+            string expected_1 = "35323 Ted theRed 16 False";
+            string expected_2 = "35323 Tina Turnbull 18 False";
+            Yucky yucky = new Yucky();
+            var employees = yucky.GetEmployees(EmployeeFilterType.ByName, "T", new FakeSqlConnection());
+            List<Employee> actual = employees.ToList();
+            Assert.IsTrue(actual.Count == 2);
+            Assert.AreEqual(expected_1, actual[0].ToString());
+            Assert.AreEqual(expected_2, actual[1].ToString());
+        }
+
+        [TestMethod()]
+        public void Program_Run_With_FilterType_Exempt_Any_Filter()
+        {
+            string expected_1 = "35323 Fred Flintstone 42 True";
+            Yucky yucky = new Yucky();
+            var employees = yucky.GetEmployees(EmployeeFilterType.ExemptOnly, null, new FakeSqlConnection());
+            List<Employee> actual = employees.ToList();
+            Assert.IsTrue(actual.Count == 1);
+            Assert.AreEqual(expected_1, actual[0].ToString());
+        }
+    }
+}
